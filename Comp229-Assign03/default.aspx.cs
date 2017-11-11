56fef081ecceca0937a9033e2592ecc7c7f3961b
Owner <Owner@LAPTOP-O8UG7H3U>
2017-11-11 00:16:26 -0500

    page setups

--- Comp229-Assign03/default.aspx.cs
+++ Comp229-Assign03/default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace Comp229_Assign03
 {
@@ -11,7 +12,22 @@ namespace Comp229_Assign03
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString.AllKeys.Contains("pro_id") && Request.QueryString.AllKeys.Contains("actin"))
+            {
+                string proID = Request.QueryString["pro_id"];
+                string action = Request.QueryString["action"];
+            }
 
+        }
+        void forDatabases(string quaries)
+        {
+            string connection = "Data Source= ; Initail Catalog=Comp229Assign03; Integrated Security=True";
+            using (SqlConnection connect = new SqlConnection(connection))
+            {
+                connect.Open();
+                SqlCommand command = new SqlCommand(quaries, connect);
+                SqlDataReader read = command.ExecuteReader();
+            }
         }
     }
 }
